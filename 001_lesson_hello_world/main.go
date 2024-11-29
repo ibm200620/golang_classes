0597e69a1ec3bc3730e5a834a5aa524d78d92a52
Gorilla Gigabytes <dave.augustus11@gmail.com>
2024-11-29 21:25:17 +0530

    Written a Hello World program
    Signed-off-by: Gorilla Gigabytes <dave.augustus11@gmail.com>

---
+++ 001_lesson_hello_world/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() { // Start the execution
+	fmt.Println("Hello, World!") // Print the message to the console
+} // This is closing statement of main function, means end the program
+
+// Note
+// - Every go program must have main.go file.
+// - Every main.go file first line should be package main.
+// - The `main` function is the entry point of the program.
